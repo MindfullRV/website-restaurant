68c17f8e10dfdeb62ff05188f370c0c68e00b0dc
Rully <vareliorullyan402@gmail.com>
2023-02-24 22:06:53 +0700

    update 24/02/23

--- index.php
+++ index.php
@@ -24,11 +24,11 @@
                     <img src="assets/svg/logo.svg" alt="icon" />
                 </a>
                 <nav>
-                    <img src="assets/svg/close.svg" alt="icon">
+                    <img class="menu" src="assets/svg/close.svg" alt="icon">
                     <ul>
-                        <li><a class="active" href="#home">Beranda</a></li>
-                        <li><a href="#about">Tentang</a></li>
-                        <li><a href="#contact">Kontak</a></li>
+                        <li><a class="active" href="#home">Home</a></li>
+                        <li><a href="#about">About</a></li>
+                        <li><a href="#contact">Contact</a></li>
                         <li>
                             <a href="login.php">
                                 <button>Login</button>
@@ -36,15 +36,17 @@
                         </li>
                     </ul>
                 </nav>
-                <img src="assets/svg/menu.svg" alt="icon">
+                <img class="menu" src="assets/svg/menu.svg" alt="icon">
             </header>
             <!-- Headline section -->
             <section id="hero">
-                <h1>Good food in good environment.</h1>
-                <p>Inspired by traditional cuisine, our menu offers fresh, local ingredients with unique flavor combinations.</p>
-                <a href="#menu">
-                    <button>Recommended Menu</button>
-                </a>
+                <div class="container">
+                    <h1>Good food in good environment.</h1>
+                    <p>Inspired by traditional cuisine, our menu offers fresh, local ingredients with unique flavor combinations.</p>
+                    <a href="#menu">
+                        <button>Our Reccomendation</button>
+                    </a>
+                </div>
             </section>
             <!-- About section -->
             <section id="about">
@@ -54,31 +56,27 @@
                         <h4>MAKES A GOOD FIRST IMPRESSION</h4>
                         <p>While food quality is very important, our customers receive their first impression of a restaurant from the welcome and appearance. Therefore, the restaurant needs to immediately start its customer service the moment the guest enters the restaurant. To do this, our employees must focus on two things, namely speaking politely, using ethics.</p>
                     </div>
-                    <img src="images/about-img1.png" alt="Image">
+                    <img src="assets/about-img1.png" alt="Image">
                 </div>
                 <div class="feature">
                     <div class="content">
                         <h4>FAST SERVICE.</h4>
                         <p>Our restaurant has enough staff to seat you, take orders, fulfill requests, clear tables and complete transactions. Otherwise, customers will be made to wait due to insufficient staff rather than increased demand.</p>
                     </div>
-                    <img src="images/about-img2.png" alt="Image">
+                    <img src="assets/about-img2.png" alt="Image">
                 </div>
                 <div class="feature flip">
                     <div class="content">
                         <h4>RESOLVE CUSTOMER COMPLAINTS IMMEDIATELY.</h4>
                         <p>Our restaurant will promptly address customer complaints to avoid bigger issues and negative reviews. Regardless of whether the complaint is legitimate or not, employees must pay attention to customers. To deal with problems, our staff will solve problems quickly and discreetly.</p>
                     </div>
-                    <img src="images/about-img3.png" alt="Image">
-                </div>
-                <div class="videos">
-                    <h1>Video</h1>
-                    <video style="width: 500px; transform: translateX(50%);" controls src="images/video.mp4"></video>
+                    <img src="assets/about-img3.png" alt="Image">
                 </div>
             </section>
             <!-- Menu section -->
             <section id="menu">
                 <div class="container">
-                    <h1>Menu Rekomendasi</h1>
+                    <h1>Our Reccomendation</h1>
                     <div class="card-container">
                         <div class="card-menu">
                             <img class="image-menu" src="assets/fried-rice.png" alt="Image">
@@ -101,9 +99,9 @@
                     </div>
                 </div>
             </section>
-            <!-- Testimony -->
+            <!-- Review Section -->
             <section id="testi">
-                <h1>Testimony</h1>
+                <h1>Reviews</h1>
                 <div class="container">
                     <div class="card">
                         <img src="assets/svg/person.svg" alt="Icon">
@@ -129,35 +127,35 @@
                         <h1>Contact</h1>
                         <div class="detail">
                             <div class="add">
-                                <img src="images/icons/ic-address.svg" alt="Icon">
+                                <img src="assets/svg/address.svg" alt="Icon">
+                                <h4>Address</h4>
+                                <p>610 W Town St, Columbus. Ohio, 43215</p>
                             <div>
-                            <h4>Address</h4>
-                            <p>610 W Town St, Columbus. Ohio, 43215</p>
-                        </div>
-                        <div class="add">
-                            <img src="images/icons/ic-phone.svg" alt="Icon">
-                            <div>
-                                <h4>Telephone</h4>
-                                <p>(614) 313-8138</p>
+                            <div class="add">
+                                <img src="assets/svg/phone.svg" alt="Icon">
+                                <div>
+                                    <h4>Telephone</h4>
+                                    <p>(614) 313-8138</p>
+                                </div>
                             </div>
-                        </div>
-                        <div class="add">
-                            <img src="images/icons/ic-email.svg" alt="Icon">
-                            <div>
-                                <h4>Email</h4>
-                                <p>restopro@restoran.com</p>
+                            <div class="add">
+                                <img src="assets/svg/email.svg" alt="Icon">
+                                <div>
+                                    <h4>Email</h4>
+                                    <p>restopro@restoran.com</p>
+                                </div>
                             </div>
-                        </div>
-                        <div class="socmed">
-                            <a href="facebook.com">
-                                <img src="images/icons/ic-facebook.svg" alt="Icon">
-                            </a>
-                            <a href="instagram.com">
-                                <img src="images/icons/ic-instagram.svg" alt="Icon">
-                            </a>
-                            <a href="twitter.com">
-                                <img src="images/icons/ic-twitter.svg" alt="Icon">
-                            </a>
+                            <div class="socmed">
+                                <a href="facebook.com">
+                                    <img src="assets/svg/facebook.svg" alt="Icon">
+                                </a>
+                                <a href="instagram.com">
+                                    <img src="assets/svg/instagram.svg" alt="Icon">
+                                </a>
+                                <a href="twitter.com">
+                                    <img src="assets/svg/twitter.svg" alt="Icon">
+                                </a>
+                            </div>
                         </div>
                     </div>
                 </div>
@@ -170,7 +168,7 @@
                         <textarea name="message" class="input" required="" autocomplete="off"></textarea>
                         <label for="message">Send Message here...</label>
                     </div>
-                    <input type="submit" class="submit"/>
+                    <input type="submit" value="Submit" class="submit"/>
                 </div>
             </section>
         </div>
@@ -178,22 +176,22 @@
         <footer>
             <div class="container">
                 <div class="footer-content">
-                    <img src="images/icons/logo.svg" alt="Icon">
+                    <img src="assets/svg/logo.svg" alt="Icon">
                     <p>Inspired by traditional cuisine, our menu offers fresh, local ingredients with unique flavor combinations.</p>
                     <ul class="social">
                         <li>
                             <a href="facebook.com">
-                                <img src="images/icons/ic-facebook.svg" alt="Icon">
+                                <img src="assets/svg/facebook.svg" alt="Icon">
                             </a>
                         </li>
                         <li>
                             <a href="instagram.com">
-                                <img src="images/icons/ic-instagram.svg" alt="Icon">
+                                <img src="assets/svg/instagram.svg" alt="Icon">
                             </a>
                         </li>
                         <li>
                             <a href="twitter.com">
-                                <img src="images/icons/ic-twitter.svg" alt="Icon">
+                                <img src="assets/svg/twitter.svg" alt="Icon">
                             </a>
                         </li>
                     </ul>
