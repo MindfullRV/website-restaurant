bc1de050e285d1d5ed358bc8954c507a65b1e670
Rully <vareliorullyan402@gmail.com>
2023-02-27 20:19:59 +0700

    update 27/02/23

--- index.php
+++ index.php
@@ -16,190 +16,27 @@
     <body>
         <!-- Image Banner -->
         <div class="hero-img"></div>
-        <!-- Wrapper -->
-        <div class="wrapper">
-            <!-- Header Nav -->
-            <header class="bg-transparent">
-                <a href="index.php">
-                    <img src="assets/svg/logo.svg" alt="icon" />
-                </a>
-                <nav>
-                    <img class="menu" src="assets/svg/close.svg" alt="icon">
-                    <ul>
-                        <li><a class="active" href="#home">Home</a></li>
-                        <li><a href="#about">About</a></li>
-                        <li><a href="#contact">Contact</a></li>
-                        <li>
-                            <a href="login.php">
-                                <button>Login</button>
-                            </a>
-                        </li>
-                    </ul>
-                </nav>
-                <img class="menu" src="assets/svg/menu.svg" alt="icon">
-            </header>
-            <!-- Headline section -->
-            <section id="hero">
-                <div class="container">
-                    <h1>Good food in good environment.</h1>
-                    <p>Inspired by traditional cuisine, our menu offers fresh, local ingredients with unique flavor combinations.</p>
-                    <a href="#menu">
-                        <button>Our Reccomendation</button>
-                    </a>
-                </div>
-            </section>
-            <!-- About section -->
-            <section id="about">
-                <h1>About Us</h1>
-                <div class="feature flip">
-                    <div class="content">
-                        <h4>MAKES A GOOD FIRST IMPRESSION</h4>
-                        <p>While food quality is very important, our customers receive their first impression of a restaurant from the welcome and appearance. Therefore, the restaurant needs to immediately start its customer service the moment the guest enters the restaurant. To do this, our employees must focus on two things, namely speaking politely, using ethics.</p>
-                    </div>
-                    <img src="assets/about-img1.png" alt="Image">
-                </div>
-                <div class="feature">
-                    <div class="content">
-                        <h4>FAST SERVICE.</h4>
-                        <p>Our restaurant has enough staff to seat you, take orders, fulfill requests, clear tables and complete transactions. Otherwise, customers will be made to wait due to insufficient staff rather than increased demand.</p>
-                    </div>
-                    <img src="assets/about-img2.png" alt="Image">
-                </div>
-                <div class="feature flip">
-                    <div class="content">
-                        <h4>RESOLVE CUSTOMER COMPLAINTS IMMEDIATELY.</h4>
-                        <p>Our restaurant will promptly address customer complaints to avoid bigger issues and negative reviews. Regardless of whether the complaint is legitimate or not, employees must pay attention to customers. To deal with problems, our staff will solve problems quickly and discreetly.</p>
-                    </div>
-                    <img src="assets/about-img3.png" alt="Image">
-                </div>
-            </section>
-            <!-- Menu section -->
-            <section id="menu">
-                <div class="container">
-                    <h1>Our Reccomendation</h1>
-                    <div class="card-container">
-                        <div class="card-menu">
-                            <img class="image-menu" src="assets/fried-rice.png" alt="Image">
-                            <div class="content-menu">
-                                <div class="text-menu">Fried Rice</div>
-                            </div>
-                        </div>
-                        <div class="card-menu">
-                            <img class="image-menu" src="assets/grilled-chicken.png" alt="Image">
-                            <div class="content-menu">
-                                <div class="text-menu">Grilled Chicken</div>
-                            </div>
-                        </div>
-                        <div class="card-menu">
-                            <img class="image-menu" src="assets/pasta.png" alt="Image">
-                            <div class="content-menu">
-                                <div class="text-menu">Pasta</div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </section>
-            <!-- Review Section -->
-            <section id="testi">
-                <h1>Reviews</h1>
-                <div class="container">
-                    <div class="card">
-                        <img src="assets/svg/person.svg" alt="Icon">
-                        <h3>John Doe</h3>
-                        <p>“The food is delicious, the seasoning taste nice. Recommended to eat with loved ones.”</p>
-                    </div>
-                    <div class="card two">
-                        <img src="assets/svg/person.svg" alt="Icon">
-                        <h3>John Doe</h3>
-                        <p>“10/10 Really a recommendation for those who want to have a date in a luxurious place, my girlfriend is so happy.”</p>
-                    </div>
-                    <div class="card">
-                        <img src="assets/svg/person.svg" alt="Icon">
-                        <h3>Jane Doe</h3>
-                        <p>“Very nice, luxurious place. The food is delicious.”</p>
-                    </div>
-                </div>
-            </section>
-            <!-- Section Contact -->
-            <section id="contact">
-                <div class="container">
-                    <div class="left">
-                        <h1>Contact</h1>
-                        <div class="detail">
-                            <div class="add">
-                                <img src="assets/svg/address.svg" alt="Icon">
-                                <h4>Address</h4>
-                                <p>610 W Town St, Columbus. Ohio, 43215</p>
-                            <div>
-                            <div class="add">
-                                <img src="assets/svg/phone.svg" alt="Icon">
-                                <div>
-                                    <h4>Telephone</h4>
-                                    <p>(614) 313-8138</p>
-                                </div>
-                            </div>
-                            <div class="add">
-                                <img src="assets/svg/email.svg" alt="Icon">
-                                <div>
-                                    <h4>Email</h4>
-                                    <p>restopro@restoran.com</p>
-                                </div>
-                            </div>
-                            <div class="socmed">
-                                <a href="facebook.com">
-                                    <img src="assets/svg/facebook.svg" alt="Icon">
-                                </a>
-                                <a href="instagram.com">
-                                    <img src="assets/svg/instagram.svg" alt="Icon">
-                                </a>
-                                <a href="twitter.com">
-                                    <img src="assets/svg/twitter.svg" alt="Icon">
-                                </a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="box">
-                    <div class="inputGroup">
-                        <input type="text" required="" autocomplete="off">
-                        <label for="name">Name</label>
-                    </div>
-                    <div class="inputGroup">
-                        <textarea name="message" class="input" required="" autocomplete="off"></textarea>
-                        <label for="message">Send Message here...</label>
-                    </div>
-                    <input type="submit" value="Submit" class="submit"/>
-                </div>
-            </section>
-        </div>
+
+        <!-- Header -->
+        <header>
+            <img src="assets/svg/logo.svg" alt="icon">
+            <nav>
+                <ul>
+                    <li><a class="active" href="#headline">Home</a></li>
+                    <li><a href="#about">About</a></li>
+                    <li><a href="#review">Review</a></li>
+                    <li><a href="#contact">Contact</a></li>
+                    <li>
+                        <a href="login.php">
+                            <button>Login</button>
+                        </a>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+        <!-- Main -->
+        <main></main>
         <!-- Footer -->
-        <footer>
-            <div class="container">
-                <div class="footer-content">
-                    <img src="assets/svg/logo.svg" alt="Icon">
-                    <p>Inspired by traditional cuisine, our menu offers fresh, local ingredients with unique flavor combinations.</p>
-                    <ul class="social">
-                        <li>
-                            <a href="facebook.com">
-                                <img src="assets/svg/facebook.svg" alt="Icon">
-                            </a>
-                        </li>
-                        <li>
-                            <a href="instagram.com">
-                                <img src="assets/svg/instagram.svg" alt="Icon">
-                            </a>
-                        </li>
-                        <li>
-                            <a href="twitter.com">
-                                <img src="assets/svg/twitter.svg" alt="Icon">
-                            </a>
-                        </li>
-                    </ul>
-                </div>
-                <div class="bottom">
-                    <p>Copyright &copy; 2023 | Made by MindfullRV</p>
-                </div>
-            </div>
-        </footer>
+        <footer></footer>
     </body>
 </html>
