596dc541db21252befbd52f90cad526faca51930
Rully <vareliorullyan402@gmail.com>
2023-03-03 23:35:46 +0700

    update 03/03/23

--- index.php
+++ index.php
@@ -36,8 +36,41 @@
         </header>
         <!-- Main -->
         <main>
+            <!-- Headline -->
             <section id="headline">
-                <div class="container"></div>
+                <div class="container">
+                    <h1>Fine dining for the modern palate</h1>
+                    <p>Inspired by traditional food, our menu offers fresh, local ingredients with unique flavor combinations.</p>
+                    <a href="#menu">
+                        <button>Our Reccomendation</button>
+                    </a>
+                </div>
+            </section>
+            <section id="about">
+                <div class="container">
+                    <h1 class="text-center">About</h1>
+                    <div class="feature flip">
+                        <div class="content">
+                            <h4>MAKES A GOOD FIRST IMPRESSION.</h4>
+                            <p>While food quality is very important, our customers receive their first impression of a restaurant from the welcome and appearance. Therefore, the restaurant needs to immediately start its customer service the moment the guest enters the restaurant. To do this, our employees must focus on two things, namely speaking politely, using ethics.</p>
+                        </div>
+                        <img src="assets/about-img1.png" alt="image">
+                    </div>
+                    <div class="feature">
+                        <div class="content">
+                            <h4>FAST AND EFFICIENT SERVICE.</h4>
+                            <p>Our restaurant has enough staff to reserve you a seat, take orders, fulfill requests, clear tables and complete transactions. Otherwise, customers will be made to wait due to insufficient staff rather than increased demand.</p>
+                        </div>
+                        <img src="assets/about-img2.png" alt="image">
+                    </div>
+                    <div class="feature flip">
+                        <div class="content">
+                            <h4>RESOLVE CUSTOMER COMPLAINTS PROMPTLY.</h4>
+                            <p>Our restaurant will promptly address customer complaints to avoid bigger issues and negative reviews. Regardless of whether the complaint is legitimate or not, employees must pay attention to customers. To deal with problems, our staff will solve problems quickly and discreetly.</p>
+                        </div>
+                        <img src="assets/about-img3.png" alt="image">
+                    </div>
+                </div>
             </section>
         </main>
         <!-- Footer -->
